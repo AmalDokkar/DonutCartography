15de6e27a7d57c14483fe74c2c3e7fea132082b6
hfoliacots <94977810+hfoliacots@users.noreply.github.com>
2022-05-03 15:05:37 +0200

    Update h.py

--- h.py
+++ h.py
@@ -4,4 +4,18 @@ import math
 R = int(input())
 r = int(input())
 
+def projectionCoordinates(theta, phi):
+  x = (R + 2*r/(math.tan(theta)+math.sec(theta))) * math.sin(phi)
+  y = (R - 2*r/(math.tan(theta)+math.sec(theta))) * math.cos(phi)
+  return x, y
+
+def getTheta(x, phi
+
+siz = 2*(R + 2*r) + 1
+
+image = Image.open('donut.jpg')
+
+img = Image.new('RGB', (siz, siz), 'Black')
+dib = ImageDraw.Draw(img)
+
 
