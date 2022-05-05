2b0b3c99e753784b02b895af2612875462e44d34
Alma <78815397+AmalDokkar@users.noreply.github.com>
2022-05-05 22:15:39 +0200

    Update a.py

--- a.py
+++ a.py
@@ -1,107 +1,125 @@
-from cmath import sqrt
 from PIL import Image, ImageDraw
-import math, mpmath
+from collections import deque
+import math
 
-# function for projection equations
 def projectionCoordinates (theta, phi):
-    # x = R * math.cos(phi) + (2*r / (math.tan(theta) + mpmath.sec(theta))) * math.sin(phi)
-    # y = R * math.sin(phi) - (2*r / (math.tan(theta) + mpmath.sec(theta))) * math.cos(phi)
     a = (2 * r * math.cos(theta)) / (1 + math.sin(theta))
     x = (R + a) * math.cos(phi)
     y = (R + a) * math.sin(phi)
     return x, y
 
-# function to get theta
 def getTheta (x, y, phi):
-    # it's all rigth because we only want positive thetas
-    # k = (x / math.cos(phi) - R) / r
-    # if (k < -1.0): k = -1.0 # because of floating point precision
-    # if (k > 1.0): k = 1.0   # this must be done
-    # theta = math.acos(k)
-    # return theta
-
     l = (math.sqrt(x*x + y*y) - R) / r
     if l > 1.0: l = 1.0
     if l < -1.0: l = -1.0
     theta = math.acos(l)
     return theta
 
-# function that return quadrant given coordinates
 def quadrant (x, y):
     if (x >= 0 and y >= 0): return 1
     elif (x < 0 and y >= 0): return 2
     elif (x < 0 and y < 0): return 3
     else: return 4
 
-# this should be given by the user and computed by hand
 R = int(input())
 r = int(input())
 
-X = int(input()) # coordinates of the center
+X = int(input())
 Y = int(input())
-# from pixels to cartesian coordinates sustract X, Y
-# cartesian coordinates to pixels add X, Y respectively
 
-# bounds of the map
-MAXX = 2*(R + 2*r)
-MAXY = 2*(R + 2*r)
+K = int(input())
 
-# output image size [2(R+2r)+1, 2(R+2r)+1]
-# dimensions [0..2(R+2r)][0..2(R+2r)]
+MAX = 2*(R + 2*r)
+
 inImg = Image.open(r"/home/amaldok/Prog/OIFem-LoC-2021-22/donut.jpg")
 
-H, W = inImg.size # height and width of input image
+H, W = inImg.size
 
-outImg = Image.new('RGB', (MAXX + 1, MAXY + 1), 'Black')
+outImg = Image.new('RGB', (MAX + 1, MAX + 1), 'Black')
 dib = ImageDraw.Draw(outImg)
 
-# interate through all pixels:
-for i in range(0, H):
-    for j in range(0, W):
+for i in range(H):
+    for j in range(W):
         
-        # cartesian coordinates
         inX = i - X
         inY = j - Y
 
-        # if quadrant(inX, inY) == 3 or quadrant(inX, inY) == 4:
-        #     continue
-
-        if r*r <= inX*inX + inY*inY <= R*R: # inside the donut
-            print(inX, inY) # to keep track of progress
-            # dib.point((inX + MAXX//2, inY + MAXY//2), 'Pink')  
+        lo, hi = R-r, R+r
+        if lo*lo <= inX*inX + inY*inY <= hi*hi:
+            print(inX, inY)
 
             phi = 10000000000
-            # if (inX < 0): signX = -1    # sadly, in only gives positive values :(
-            # if (inY < 0): signY = -1
-            if (inX != 0.0): phi = math.atan(inY / inX)   # not 100% sure about this
+            if (inX != 0.0): phi = math.atan(inY / inX)
 
             theta = getTheta(inX, inY, phi)
-            # print(theta)
             outX, outY = projectionCoordinates(theta, phi)
 
-            outX += MAXX//2   # important to add the center coordinates
-            outY += MAXY//2   # to get the pixel's position!
+            if quadrant(inX, inY) == 1 or quadrant(inX, inY) == 4:
+                outX += MAX//2
+                outY += MAX//2
+                if 0 <= outX <= MAX and 0 <= outY <= MAX:
+                    color = inImg.getpixel((i, j))
+                    dib.point((outX, outY), color)
+            else:
+                outX = MAX//2 - outX
+                outY = MAX//2 - outY
+                if 0 <= outX <= MAX and 0 <= outY <= MAX:
+                    color = inImg.getpixel((i, j))
+                    dib.point((outX, outY), color)
 
-            if 0 <= outX <= MAXX and 0 <= outY <= MAXY: # inside bounds
-                color = inImg.getpixel((i, j))
-                dib.point((outX, outY), color) # dib on outImg
+bfs = deque()
+u, v = [1, 0, -1, 0], [0, 1, 0, -1]
+
+def black (x, y):
+    r, g, b = outImg.getpixel((x, y))
+    if r == 0 and g == 0 and b == 0:
+        return True
+    return False
+
+def hasUncoloredNeighbours (x, y):
+    for i in range(4):
+        xi = x + u[i]
+        yi = y + v[i]
+        if (0 <= xi <= MAX and 0 <= yi <= MAX and black(xi, yi)):
+            return True
+    return False
+
+
+for i in range(MAX+1):
+    for j in range(MAX+1):
+        if hasUncoloredNeighbours(i, j):
+            color = outImg.getpixel((i, j))
+            bfs.append((i, j, color))
+
+while len(bfs) > 0:
+    x, y, color = bfs.pop()
+    if black(x, y):
+        dib.point((x, y), color)
+
+    for i in range(4):
+        xi = x + u[i]
+        yi = y + v[i]
+        if (0 <= xi <= MAX and 0 <= yi <= MAX and black(xi, yi)):
+            bfs.append((x, y, color))
     
-outImg.save("out18.jpg")
+outImg.save("out22.jpg")
 
 
 """
 
 Test 1:
-    R = 725
-    r = 220
-    X = 745
-    Y = 725
+    R = 505
+    r = 222
+    X = 726
+    Y = 726
+    K = 123
 
 c/p:
 
-725
-220
-745
-725
+505
+222
+726
+746
+123
 
 """
